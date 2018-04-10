1f586d607779f94d97eaf29a807a901c0340f9e8
cyko <cyko@yahoo-inc.com>
2018-04-10 09:58:27 +0800

    Initial Commit

---
+++ Train0410/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Train0410
+//
+//  Created by  Chih-Yu Ko on 2018/4/10.
+//  Copyright © 2018年 Oath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Train0410/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Train0410
+//
+//  Created by  Chih-Yu Ko on 2018/4/10.
+//  Copyright © 2018年 Oath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
